d3db2393fa4305ad55517c9f9fd8a0b6e3c12e07
Cleyton <cleytonbc@hotmail.com>
2025-06-02 19:31:21 -0300

    feat: adicionado validação de variável

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvModule } from './infra/env/env.module';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './infra/env/env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
