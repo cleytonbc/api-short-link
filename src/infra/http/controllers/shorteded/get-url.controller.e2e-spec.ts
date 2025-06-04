f031cbc4c8e36728bc34e5a9c204322ac9d9afce
Cleyton <cleytonbc@hotmail.com>
2025-06-04 17:05:16 -0300

    feat: implementado redirecionamento da url pelo link encurtado

---
+++ src/infra/http/controllers/shorteded/get-url.controller.e2e-spec.ts
@@ -0,0 +1,55 @@
+import type { Server } from 'http';
+import { describe, expect } from 'vitest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { AppModule } from '@/app.module';
+import { ShortenedUrlFactory } from '@test/factories/make-shortened-url';
+
+describe('Acessar url encurtada (E2E)', () => {
+  let app: INestApplication;
+  let prismaService: PrismaService;
+  let shortenedUrlFactory: ShortenedUrlFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+      providers: [ShortenedUrlFactory]
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prismaService = moduleRef.get<PrismaService>(PrismaService);
+    shortenedUrlFactory = moduleRef.get(ShortenedUrlFactory);
+
+    await app.init();
+
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test('[GET] /:code', async () => {
+
+    const shortenedUrl = await shortenedUrlFactory.makePrismaUser()
+
+    expect(shortenedUrl.clickCount).toBe(0);
+
+    const code = shortenedUrl.shortCode
+    const response = await request(app.getHttpServer() as Server)
+    .get(`/${code}`)
+    .expect(302);
+    
+    expect(response.headers.location).toBe(shortenedUrl.originalUrl);
+
+    const afterClick = await prismaService.shortenedUrl.findUnique({
+      where: { id: shortenedUrl.id.toString() },
+    });
+
+    expect(afterClick?.clickCount).toBe(1);
+
+  }, 60000);
+
+
+});
--- src/infra/http/controllers/shorteded/shorteded.module.ts
+++ src/infra/http/controllers/shorteded/shorteded.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { CreateShortenedUrlUseCase } from '@/domain/shortener/use-cases/create-shortened-url';
 import { CreateShortenedUrlController } from './create-shortened-url.controller';
 import { GenerateUniqueShortCodeService } from '@/domain/shortener/services/generate-unique-short-code.service';
+import { GetUrlController } from './get-url.controller';
+import { GetShortenedUrlUseCase } from '@/domain/shortener/use-cases/get-shortened-url';
 
 @Module({
-  controllers: [CreateShortenedUrlController, ],
+  controllers: [CreateShortenedUrlController, GetUrlController],
   providers: [
     CreateShortenedUrlUseCase,
     GenerateUniqueShortCodeService,
+    GetShortenedUrlUseCase
   ],
 })
 export class ShortedModule {}
