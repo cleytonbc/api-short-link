a1060f7a919478b74de83837b184efbf75ae1bdf
Cleyton <cleytonbc@hotmail.com>
2025-06-08 12:46:00 -0300

    feat:  adicionando tratamento de erro

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import './infra/telemetry/telemetry';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './infra/swagger/swagger.config';
+import { AllExceptionsFilter } from './infra/http/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   setupSwagger(app);
 
