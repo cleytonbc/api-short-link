dbdedc43d76a1bed3eb84c880781b897144dc4e0
Cleyton <cleytonbc@hotmail.com>
2025-06-08 12:24:34 -0300

    chore: melhorado mensagem de erro e descrição no swagger ao criar uma url

--- src/infra/http/controllers/dtos/request/create-shortened-url-request-dto.ts
+++ src/infra/http/controllers/dtos/request/create-shortened-url-request-dto.ts
@@ -2,7 +2,14 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 const CreateShortenedUrlRequesSchema = z.object({
-  url: z.string().url().describe('Endereço URL'),
+  url: z
+    .string()
+    .url({
+      message: '"Please enter a valid URL, including https:// or http://." ',
+    })
+    .describe(
+      'Endereço URL válido (Certifique-se de incluir https:// ou http://): exemplo https://www.google.com',
+    ),
 });
 
 export class CreateShortenedUrlRequesDto extends createZodDto(
